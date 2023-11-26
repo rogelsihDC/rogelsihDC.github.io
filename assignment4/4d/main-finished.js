634a4f98249ac8261aa149d03f386159b7eb78e1
rogelsihDC <rogeljames.sih@dcmail.ca>
2023-11-25 23:42:07 -0500

    Update main-finished.js

--- assignment4/4d/main-finished.js
+++ assignment4/4d/main-finished.js
@@ -66,19 +66,39 @@ class Ball {
 
    collisionDetect() {
       for (const ball of balls) {
-         if (!(this === ball)) {
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
-            }
-         }
+        if (!(this === ball) && ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+          }
+        }
       }
-   }
+    }
 
 }
+// Shape Class
+class Shape {
+   constructor(x, y, velX, velY) {
+     this.x = x;
+     this.y = y;
+     this.velX = velX;
+     this.velY = velY;
+   }
+ }
+
+ // Ball class derived from Shape
+class Ball extends Shape {
+   constructor(x, y, velX, velY, size, color) {
+     super(x, y, velX, velY);
+     this.color = color;
+     this.size = size;
+     // used to track wether the ball exists
+     this.exists = true;
+   }
+ }
 
 const balls = [];
 

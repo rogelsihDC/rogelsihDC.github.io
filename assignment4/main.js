b5d5e2bcec311939893ca65b9c315fd84e08c52e
rogelsihDC <rogeljames.sih@dcmail.ca>
2023-11-25 20:36:01 -0500

    Create main.js

---
+++ assignment4/main.js
@@ -0,0 +1,29 @@
+// Rogel Sih
+// Assignment 4 - Silly Stories
+// INFT1206 - Web Development Fundamentals
+// An HTML file to demostrate what we have learned in JavaScript.
+// There is a button, where if you click, it will generate a random silly story from the text file
+
+
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertX = 
+  [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"
+  ];
+  
+  const insertY = 
+  [
+    "the soup kitchen",
+    "Disneyland",
+    "the White House"
+  ];
+  
+  const insertZ =
+   [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",s
+    "turned into a slug and crawled away"
+  ];
